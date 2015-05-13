7c7e5f93b7cc62773b403a3d0526e16ec9066b2f
Harald Wellmann <>
2015-05-13 22:22:32 +0200

    Add parallelized test executor

---
+++ src/test/java/Parallelized.java
@@ -0,0 +1,52 @@
+import org.junit.runners.Parameterized;
+import org.junit.runners.model.RunnerScheduler;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Taken from:
+ * http://hwellmann.blogspot.pt/2009/12/running-parameterized-junit-tests-in.html
+ */
+public class Parallelized extends Parameterized
+{
+
+    private static class ThreadPoolScheduler implements RunnerScheduler
+    {
+        private ExecutorService executor;
+
+        public ThreadPoolScheduler()
+        {
+            String threads = System.getProperty("junit.parallel.threads", "16");
+            int numThreads = Integer.parseInt(threads);
+            executor = Executors.newFixedThreadPool(numThreads);
+        }
+
+        @Override
+        public void finished()
+        {
+            executor.shutdown();
+            try
+            {
+                executor.awaitTermination(10, TimeUnit.MINUTES);
+            }
+            catch (InterruptedException exc)
+            {
+                throw new RuntimeException(exc);
+            }
+        }
+
+        @Override
+        public void schedule(Runnable childStatement)
+        {
+            executor.submit(childStatement);
+        }
+    }
+
+    public Parallelized(Class klass) throws Throwable
+    {
+        super(klass);
+        setScheduler(new ThreadPoolScheduler());
+    }
+}
